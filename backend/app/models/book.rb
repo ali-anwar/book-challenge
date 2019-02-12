fb893a7443941fb13cacc5405ced0d9599aa7169
Ali Anwar <ali@alis-mac.local>
2019-02-12 14:09:00 +0500

    Book: Added validations

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :isbn, presence: { message: "ISBN can't be blank" }
+  validates :isbn, length: { in: 10..17 }
+  validates :title, presence: { message: "Title can't be blank" }
 end
