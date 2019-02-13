5889398053edfb05481ed4850003c5287f233df1
Ali Anwar <ali@alis-mac.local>
2019-02-13 12:33:33 +0500

    Book: Updated model validation messages

--- backend/app/models/book.rb
+++ backend/app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  validates :isbn, presence: { message: "ISBN can't be blank" }
+  validates :isbn, presence: true
   validates :isbn, length: { in: 10..17 }
-  validates :title, presence: { message: "Title can't be blank" }
+  validates :title, presence: true
 end
